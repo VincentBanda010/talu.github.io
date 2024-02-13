2c4d59c78651466e6d9e334f3f5b96f02a2c3822
vincentbanda0 <vincentbanda010@gmail.com>
2024-02-13 15:41:08 -0800

    Love: Brandina the girl I love

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+// script.js
 function handleYes() {
-    alert('Youre my Valentine! 💖');
+    alert("You're my Valentine! 💖");
 }
 
 function handleNo() {
     var yesButton = document.getElementById('yesButton');
     var noButton = document.getElementById('noButton');
-    var mainSection = document.querySelector('.content-section');
+    var buttonContainer = document.querySelector('.button-container');
 
-    // Swap the positions of the Yes and No buttons
-    mainSection.insertBefore(noButton, yesButton);
+    // Swap the positions of the Yes and No buttons within the button container
+    buttonContainer.insertBefore(noButton, yesButton.nextSibling);
+
+    // Swap the IDs to maintain functionality
+    yesButton.id = 'noButton';
+    noButton.id = 'yesButton';
 
     // Alert the user to click Yes after swapping buttons
     alert('Oh no! Please reconsider and click Yes. 🥺');
